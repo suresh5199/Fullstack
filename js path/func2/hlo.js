1814da83a68338a523bb14df51d025de33bd5b8d
suresh5199 <sureshreddyrv1@gmail.com>
2021-08-20 06:15:24 +0100

    sample

---
+++ js path/func2/hlo.js
@@ -0,0 +1,8 @@
+
+function cal_Age(name, year) {
+    return 2021 - year;
+    console.log(Name);
+  }
+  
+  let result = cal_Age("Rahul", 1985);
+  console.log(result);
---
+++ js path/two.js
@@ -0,0 +1,2 @@
+let n=20;
+console.log(typeof n);
